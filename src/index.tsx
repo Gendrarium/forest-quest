b08f71e7fb35de5803b005d4e19700bd623c47b0
Narek Manukyan <gendrarium@yandex.ru>
2023-03-12 23:00:41 +0300

    change browserRouter to HashRouter for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.scss';
 import App from './components/App/App';
 
@@ -9,8 +9,8 @@ const rootElement = document.getElementById('root') as HTMLDivElement;
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
